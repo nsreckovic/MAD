7760004c2678522156dd32f3e4a4d80c4d21e62f
nsreckovic <sreckonikola@gmail.com>
2020-04-18 00:59:11 +0200

    Added Sign In page

---
+++ app/src/main/java/com/ns/mad_p1/view/activities/LoginActivity.kt
@@ -0,0 +1,59 @@
+package com.ns.mad_p1.view.activities
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.ns.mad_p1.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+
+    companion object {
+        const val default_pin = 1234
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        signinBtn.setOnClickListener {
+            val editor = getSharedPreferences(packageName, Context.MODE_PRIVATE).edit()
+            val name = nameEt.text.toString()
+            val surname = surnameEt.text.toString()
+            val hospital = hospitalEt.text.toString()
+            val pin_str = pinEt.text.toString()
+
+            if (name == "" || surname == "" || hospital == "" || pin_str == "") {
+                Toast.makeText(this, R.string.input_text_error, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val pin = Integer.parseInt(pin_str)
+
+            if (pin_str.length != 4) {
+                Toast.makeText(this, R.string.input_pin_lenght_error, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (pin == default_pin) {
+                editor.putBoolean(SplashActivity.LOGGED_IN_KEY, true)
+                editor.putString(SplashActivity.LOGGED_IN_USER, name)
+                editor.commit()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, R.string.input_pin_error, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/ns/mad_p1/view/activities/SplashActivity.kt
+++ app/src/main/java/com/ns/mad_p1/view/activities/SplashActivity.kt
@@ -1,15 +1,31 @@
 package com.ns.mad_p1.view.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
+    companion object {
+        const val LOGGED_IN_KEY = "loggedInKey"
+        const val LOGGED_IN_USER = "loggedInUser"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+
+        val sharedPreferences = getSharedPreferences(packageName, Context.MODE_PRIVATE)
+        val loggedIn = sharedPreferences.getBoolean(LOGGED_IN_KEY, false)
+
+        if (loggedIn) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         finish()
     }
 }
