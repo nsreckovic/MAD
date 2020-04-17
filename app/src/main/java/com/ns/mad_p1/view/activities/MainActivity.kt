2e3a11d1f0aed39dbabcab2bda4385ade4471b68
nsreckovic <sreckonikola@gmail.com>
2020-04-17 22:45:49 +0200

    Added splash screen

--- app/src/main/java/com/ns/mad_p1/view/activities/MainActivity.kt
+++ app/src/main/java/com/ns/mad_p1/view/activities/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.ns.mad_p1
+package com.ns.mad_p1.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ns.mad_p1.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
